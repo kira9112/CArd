4a654fab0ffe9d2c421ff58e8323245294efd656
Kira9112 <kira91_911@mail.ru>
2021-06-10 13:02:11 +0300

    Chanded report.md and scr

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "4024007136283292762";
+        String number = "5893574687035983";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
